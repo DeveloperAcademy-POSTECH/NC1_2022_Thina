dd5f7673006d238f72ba90e9048be4edc66dd8ac
Jungin Yoo <frog7134@gmail.com>
2022-04-28 17:50:29 +0900

    Initial Commit

---
+++ SoccerAbroad/SoccerAbroadApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerAbroadApp.swift
+//  SoccerAbroad
+//
+//  Created by 유정인 on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct SoccerAbroadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
